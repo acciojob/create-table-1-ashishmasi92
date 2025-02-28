738ed328e0b6d7b507e51002d772ba62734485d2
Ashish <ashishmasih92g@gmail.com>
2025-02-28 09:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+
+let table = document.querySelector("#sampleTable");
+
+
 function insert_Row() {
-    //Write your code here
+    let createTr = document.createElement("tr")
+let count=0
+	for(let i=0;i<2;i++){
+	let creatTd = document.createElement("td")
+		count++
+				creatTd.innerText = "New Cell"+count
+
+		createTr.append(creatTd)
+		
+		
+	}
+
+	table.prepend(createTr)
+	
+
+	
   
   
 }
